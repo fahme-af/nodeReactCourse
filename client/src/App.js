02084d5023430f1f8db5886251be63c5a1d822c4
fahmedx <fahmecanaldx@gmail.com>
2022-07-25 00:32:31 -0300

    Added first Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes as Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes as Switch, Link } from "react-router-dom"
 import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return <div className="App">
     <Router>
+      <Link to="/">Home</Link>
+      <Link to="/create-post">Create A Post</Link>
       <Switch>
         <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Switch>
     </Router>
   </div>;
